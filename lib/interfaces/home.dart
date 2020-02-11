a5a4fe23d174983781fed58bb91ffb7dc5feff4a
Mohamed Abbassi <mabbassix3@gmail.com>
2020-02-11 12:31:02 +0100

    create the first screen of the app

--- lib/interfaces/home.dart
+++ lib/interfaces/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+
 
 class HomeScreen extends StatelessWidget{
   @override
@@ -11,6 +15,35 @@ class HomeScreen extends StatelessWidget{
       body: Container(
         child: Column(
           children: <Widget>[
+            Text(
+              'Now',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 50,
+              ),
+            ),
+            CarouselSlider(
+              height: 200.0,
+              items: [1, 2, 3].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                        width: MediaQuery
+                            .of(context)
+                            .size
+                            .width,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(
+                            color: Colors.amber
+                        ),
+                        child: Text(
+                          'text $i', style: TextStyle(fontSize: 16.0),)
+                    );
+                  },
+                );
+              }).toList(),
+            ),
 
           ],
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:me_daily/interfaces/home.dart';
 void main() => runApp(MyApp());
 
 /// This Widget is the main application widget.
@@ -12,86 +12,53 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: _title,
       debugShowCheckedModeBanner: false,
-      home: MyStatefulWidget(),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3 :Profile',
-      style: optionStyle,
-    )
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Me Daily'),
-        backgroundColor: Colors.deepPurple,
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home,
-              color: Colors.deepPurpleAccent
-            ),
-            title: Text('Home'),
+      home: DefaultTabController(
+        length: 4,
+        child: new Scaffold(
+          body: TabBarView(
+            children: <Widget>[
+              new Container(
+                //color: Colors.yellow,
+                child: HomeScreen(),
+              ),
+              new Container(
+                color: Colors.orange,
+              ),
+              new Container(
+                color: Colors.lightGreen,
+              ),
+              new Container(
+                color: Colors.red,
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business,
-            color: Colors.deepPurpleAccent,),
-            title: Text('Business'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school,
-            color: Colors.deepPurpleAccent,),
-            title: Text('School'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_pin,
-            color: Colors.deepPurpleAccent,),
-            title: Text('Profile'),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.deepPurpleAccent,
-        onTap: _onItemTapped,
+          bottomNavigationBar: new TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: new Icon(Icons.home),
+              ),
+              Tab(
+                icon: new Icon(Icons.calendar_today),
+              ),
+              Tab(
+                icon: new Icon(Icons.alarm),
+              ),
+              Tab(
+                icon: new Icon(Icons.person),
+              )
 
+            ],
+            labelColor: Colors.yellow,
+            unselectedLabelColor: Colors.blue,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorPadding: EdgeInsets.all(5.0),
+            indicatorColor: Colors.red,
+          ),
+          backgroundColor: Colors.black,
+        ),
       ),
     );
   }
 }
+
+
