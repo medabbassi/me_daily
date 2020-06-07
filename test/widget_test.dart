7c41732ed666e0b1270d98ca84247c6f58ef8c11
mohamed abbassi <mabbassix@gmail.com>
2020-06-07 19:00:26 +0100

    make the intro screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:me_daily/main.dart';
+import 'package:me_daily/interfaces/IntroScreen/intro.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(BottomNavBar());
+    await tester.pumpWidget(IntroScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
