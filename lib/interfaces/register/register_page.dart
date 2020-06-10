8197ea1f5fc26605553f46c5de389cff62034b75
Mohamed Abbassi <mabbassix3@gmail.com>
2020-06-10 17:23:54 +0100

    dev_local

--- lib/interfaces/register/register_page.dart
+++ lib/interfaces/register/register_page.dart
@@ -1,10 +1,7 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:hive/hive.dart';
 import 'package:me_daily/interfaces/login/login_page.dart';
-import 'package:path_provider/path_provider.dart';
 
 // Uncomment if you use injector package.
 // import 'package:my_app/framework/di/injector.dart';
@@ -30,12 +27,7 @@ class RegisterState extends State<Register> {
 
   Box<String> usersBox;
 
-  void _OnPressed() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    Directory document = await getApplicationDocumentsDirectory();
-    Hive.init(document.path);
-    await Hive.openBox<String>("Users");
-  }
+  void _OnPressed() {}
 
   @override
   void initState() {
